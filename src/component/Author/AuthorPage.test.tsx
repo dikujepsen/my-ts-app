5f25802e11593813e8a6ed35e0d8d68c484effac
jacob <jepsenjacob@gmail.com>
2018-03-04 12:26:08 +0100

    Add missing types packages and test that checks if default string is inserted in input

--- src/component/Author/AuthorPage.test.tsx
+++ src/component/Author/AuthorPage.test.tsx
@@ -1,20 +1,26 @@
 import { AuthorPage, AuthorForm } from '.';
 import * as React from "react";
-import { shallow, configure } from "enzyme";
+import { shallow, configure, render, mount } from "enzyme";
 import * as Adapter from "enzyme-adapter-react-16";
-import {Helmet} from 'react-helmet';
+import { Helmet } from 'react-helmet';
 
 configure({ adapter: new Adapter() });
 
 describe("test suite 1", () => {
   it("should contain the correct components", () => {
     const authorPageWrapper = shallow(<AuthorPage />);
-    
+
     //expect(global.window.document.title).toBe("Author page");
     // expect(authorPageWrapper).toContain("<div>");
     expect(authorPageWrapper.find(AuthorForm)).toHaveLength(1);
     expect(authorPageWrapper.find(Helmet)).toHaveLength(1);
-    
+
   });
+
+  it('rendered the test first name', () => {
+    const wrapper = mount(<AuthorPage />);
+    expect(wrapper.find("[name='firstName']").first().props().value).toContain('Jens');
+  });
+
 });
 
