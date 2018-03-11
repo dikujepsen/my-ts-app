d4acd9842c0b696d8132730391f692226eab2a7d
jacob <jepsenjacob@gmail.com>
2018-03-11 14:09:46 +0100

    add script for running test and add test on author form

--- src/component/Author/AuthorPage.test.tsx
+++ src/component/Author/AuthorPage.test.tsx
@@ -1,9 +1,10 @@
-import { AuthorPage, AuthorList } from '.';
+import { AuthorPage, AuthorList, AuthorForm } from '.';
 import * as React from "react";
 import { shallow, configure, render, mount } from "enzyme";
 import * as Adapter from "enzyme-adapter-react-16";
 import { Helmet } from 'react-helmet';
 import { MemoryRouter as Router } from 'react-router-dom';
+import { IAuthor } from 'interface';
 
 const mountWithRouter = (node: React.ReactElement<any>) => mount(<Router>{node}</Router>);
 
@@ -19,7 +20,6 @@ describe("test suite 1", () => {
   });
 
   it('rendered a line with data', () => {
-    const context = { router: { isActive: (a: any, b: any) => true } };
 
     const wrapper = mountWithRouter(<AuthorPage />);
 
@@ -27,13 +27,22 @@ describe("test suite 1", () => {
   });
 
  
-  // it('rendered the test first name', () => {
-  //   const context = { router: { isActive: (a: any, b: any) => true } };
+  it('rendered the test first name', () => {
+    const author: IAuthor = { firstName: "Jens", lastName: "Hansen", id: 1 };
+    const errors: IAuthor = { firstName: "", lastName: "", id: -1 };
 
-  //   const wrapper = mountWithRouter(<AuthorPage />);
+    const wrapper = mount(
+      <AuthorForm 
+        author={author}
+        onSave={() => undefined}
+        onChange={() => undefined}
+        saving={false}
+        errors={errors}
+      />
+    );
 
-  //   expect(wrapper.find("[name='firstName']").first().props().value).toContain('Jens');
-  // });
+    expect(wrapper.find("[name='firstName']").first().props().value).toContain('Jens');
+  });
 
 
 
