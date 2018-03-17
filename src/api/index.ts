119b4b3167f722920c5162fbb8b0e0c877177b11
jacob <jepsenjacob@gmail.com>
2018-03-17 09:40:48 +0100

    Add scripts for changing between using static mockapi and the development backend when developing

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,10 +5,9 @@ import { authorApi as realAuthorApi } from "./AuthorApi";
 
 let authorApi: IRestApi<Author>;
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "development" && process.env.REACT_APP_MOCK_API === "static") {
     authorApi = mockAuthorApi;
 } else {
     authorApi = realAuthorApi;
 }
-authorApi = realAuthorApi;
 export { authorApi };
